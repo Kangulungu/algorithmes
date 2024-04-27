f4e644bb91eee8f04d78660f800a6c3c7cb41913
Pite Kangulungu <kangulungu@gmail.com>
2024-04-27 23:33:11 +0200

    Add project files.

---
+++ Associative_Arrays/A.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Associative_Arrays
+{
+    public class A
+    {
+        private double d, e, f;
+
+        public A(double v)
+        {
+            d = e = f = v;
+        }
+
+        public double this[string str]
+        {
+            get
+            {
+                switch (str)
+                {
+                    case "d": { return d; }
+                    case "e": { return e; }
+                    case "f": { return f; }
+                    default: throw new Exception("Error");
+                }
+            }
+            set
+            {
+                switch (str)
+                {
+                    case "d": { d = value; break; }
+                    case "e": { e = value; break; }
+                    case "f": { f = value; break; }
+                    default: throw new Exception("Error");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return "A: " + d + ", " + e + ", " + f;
+        }
+
+    }
+}
---
+++ Associative_Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Associative_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A(5);
+            double d;
+
+            a["d"] = 6; a["e"] = 7.0; a["f"] = 8.0;
+
+            d = a["e"] + a["f"];   // corresponds to d = a.d + a.e 
+                                   // in case d and e had been public
+
+            Console.WriteLine("a: {0}, d: {1}", a, d);
+            Console.ReadLine();
+        }
+    }
+}
